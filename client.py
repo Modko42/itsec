94a8e6091df2c38417408109dbb3a918235569e7
Suciu Barna <szubar10@gmail.com>
2021-04-18 14:20:30 +0200

    creating protocolyzer and developing client

--- client.py
+++ client.py
@@ -1,10 +1,21 @@
-import cr
+import Crypto
+from netinterface import network_interface
+from Crypto.Protocol.KDF import PBKDF2
+from Crypto.Random import get_random_bytes
+from Crypto.Hash import SHA512
+from protocolyzer import Protocolyzer
+
+
+NET_PATH = './'
+OWN_ADDR = 'A'
+netint = network_interface(NET_PATH, OWN_ADDR)
 
 def init_connection():
     #all the steps required to set up the secure channel between the client and server
     #generate custom key
     #encrypt the public key with it
     print('hello')
+    proto = Protocolyzer()
 
 
 def get_password():
@@ -12,4 +23,12 @@ def get_password():
     password = input()
     return password
 
-def 
+def login():
+    print("Starting login process...")
+    passwd = get_password()
+    netint.send_msg('B', proto.protocolyze(passwd))
+    salt = get_random_bytes(16)
+    file_key = PBKDF2(passwd, salt, 32, count=1000000, hmac_hash_module=SHA512)
+
+
+login()
---
+++ protocolyzer.py
@@ -0,0 +1,6 @@
+class Protocolyzer():
+    def __init__(self, key):
+        self.key = key
+
+    def protocolyze(message):
+        print("Hello, I am protocolyzer")
