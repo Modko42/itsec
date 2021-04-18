a6bd5f540fb88e62702badb37544bd01d3d3ed3e
Suciu Barna <szubar10@gmail.com>
2021-04-18 13:23:36 +0200

    creating client

---
+++ client.py
@@ -0,0 +1,15 @@
+import cr
+
+def init_connection():
+    #all the steps required to set up the secure channel between the client and server
+    #generate custom key
+    #encrypt the public key with it
+    print('hello')
+
+
+def get_password():
+    print("Please provide your password:")
+    password = input()
+    return password
+
+def 
