26d55be8d8fdd06824d8bb10ecf4684659f8cfc2
Modko42 <beni1998@hotmail.hu>
2021-04-18 16:17:32 +0200

    protocolyzer v1.1

--- protocolyzer.py
+++ protocolyzer.py
@@ -1,7 +1,15 @@
 from Crypto.Cipher import AES
+from Crypto.Hash import HMAC, SHA256
+from Crypto.Random import get_random_bytes
+
+#TODO
+secret_key = get_random_bytes(16) 
+public_key = get_random_bytes(16)
+private_key = get_random_bytes(32) 
+#TODO
 
 class Message:
-    def __init(self,version=1,type=1,len=4,seq=1,timestamp=1,max=1,slice=0,data,padding=0):
+    def __init(self,version=1,type=1,len=4,seq=1,timestamp=1,max=1,slice=0,padding=0,data):
         self.version = version
         self.type = type
         self.len = len
@@ -11,16 +19,31 @@ class Message:
         self.slice = slice
         self.data = data
         self.padding = padding
-    def convert_to_bytes():
-        return bytes(version,'utf-8')+bytes(type,'utf-8')+bytes(len,'utf-8')+bytes(seq,'utf-8')+
-        bytes(timestamp,'utf-8')+bytes(max,'utf-8')+bytes(slice,'utf-8')+bytes(data,'utf-8')+bytes(padding,'utf-8')
+ 
+    def calculateMAC(self):
+        h = HMAC.new(secret_key,digestmod=SHA256)
+        h.update(bytearray)
+        return h.hexdigest()
+        #Mint kiderült ez nem kell, mert a EAX mód alapból használ mac-et, ami a tag-be kerül
+ 
+    def convert_to_bytes(self):
+        bytearray =  bytes(version,'utf-8')+bytes(type,'utf-8')+bytes(len,'utf-8')+bytes(seq,'utf-8')+bytes(timestamp,'utf-8')+bytes(max,'utf-8')+bytes(slice,'utf-8')+bytes(data,'utf-8')+bytes(padding,'utf-8')
+        return bytearray
+    
+    def message_from_bytes(self,bytearray):
+
 
 class Protocolyzer:
     def __init__(self, key):
         self.key = key
 
-    def protocolyze(message):
+    def protocolyze(self,message):
         cipher = AES.new(self.key,AES.MODE_EAX)
-        cipher_text, tag = cipher.encrypt_and_digest()
+        cipher_text, tag = cipher.encrypt_and_digest(message.convert_to_bytes())
+        return cipher.nonce,tag,cipher_text
 
-    def undedo_protocolyze()
+    def deprotocolyze(self,nonce,tag,cipher_text)
+        cipher = AES.new(key,AES.MODE_EAX,nonce)
+        data = cipher.decrypt_and_verify(cipher_text,tag)
+        return message_from_bytes(data)
+
