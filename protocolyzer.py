afce860019c662aa0c60517f2a17e5cd21a9ef46
Modko42 <beni1998@hotmail.hu>
2021-04-18 15:14:29 +0200

    protocolyzer update

--- protocolyzer.py
+++ protocolyzer.py
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
-=======
+from Crypto.Cipher import AES
+
 class Message:
     def __init(self,version=1,type=1,len=4,seq=1,timestamp=1,max=1,slice=0,data,padding=0):
         self.version = version
@@ -11,13 +11,16 @@ class Message:
         self.slice = slice
         self.data = data
         self.padding = padding
+    def convert_to_bytes():
+        return bytes(version,'utf-8')+bytes(type,'utf-8')+bytes(len,'utf-8')+bytes(seq,'utf-8')+
+        bytes(timestamp,'utf-8')+bytes(max,'utf-8')+bytes(slice,'utf-8')+bytes(data,'utf-8')+bytes(padding,'utf-8')
 
->>>>>>> 284a1f523eb3a8c1deda4a5d69b7086a93870aad
 class Protocolyzer:
     def __init__(self, key):
         self.key = key
 
     def protocolyze(message):
-        print("Hello, I am protocolyzer")
+        cipher = AES.new(self.key,AES.MODE_EAX)
+        cipher_text, tag = cipher.encrypt_and_digest()
 
     def undedo_protocolyze()
